5bb399409fb5bfaa65aefc7d68718af19357323d
lemonjai <leocheung_1986@hotmail.com>
2016-11-26 23:01:41 -0500

    initial app

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destory
+  end
+
+  private
+    def post_params
+    end
+
+end
