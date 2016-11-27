c66eda356f649c38f5a2269ea0859a555aa6743c
lemonjai <leocheung_1986@hotmail.com>
2016-11-26 23:34:26 -0500

    add the functions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
 
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
+    
   end
 
   def new
@@ -8,19 +15,37 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def edit
   end
 
-  def destory
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
+    def find_post
+      @post = Post.find(params[:id])
+    end
+
     def post_params
+      params.require(:post).permit(:title, :desc, :link)
     end
 
 end
