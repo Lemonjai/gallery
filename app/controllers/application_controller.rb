338841f2a196edf90a10c2236b6c2320873fcc6f
lemonjai <leocheung_1986@hotmail.com>
2016-12-06 22:42:34 -0500

    add first and last name to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # Modifying the devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :lname])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:fname, :lname])
+    end
 end
